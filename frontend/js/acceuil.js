41f2d17ff1afffad3fac12b65b92e38d257eca6c
Frederic Robert <fredrob622@free.com>
2025-03-09 18:46:28 +0100

    version https

--- frontend/js/acceuil.js
+++ frontend/js/acceuil.js
@@ -1,4 +1,4 @@
-const cercle = document.querySelector('.cercle');
+// const cercle = document.querySelector('.cercle');
 const imgs = document.querySelectorAll('img');
 const titreH1 = document.querySelector('h1');
 
@@ -9,7 +9,7 @@ TL.to(imgs, {scale: 1, duration: 0.1,  stagger: 0.1});
 
 // TweenMax.from(titreH1, 1, {scale: 0, width:"400", x:"400",  opacity:1, ease: "elastic.out(1, 0.7)" });
 // TweenMax.from(titreH1, 1, {scale: 4, width:"200", x:"400", opacity:0, delay:1});
-TweenMax.to(titreH1, 1, {scale: 1, width:"400",  opacity:0, delay:1});
+TweenMax.to(titreH1, 1, {scale: 1, width:"300",  opacity:0, delay:1});
 TweenMax.to(titreH1, 1, {scale: 1, x:"100",  opacity:1, delay:2});
 TweenMax.to(titreH1, {scale:1, transformOrigin:"0% 100%", duration: 4});
 
